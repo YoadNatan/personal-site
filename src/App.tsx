20766ddb7312c2f70f0fca7d4b6aac08683d30b4
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-03-16 13:41:44 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,58 @@
-import React from 'react';
-import { Box, Typography, Divider } from '@mui/material';
+import React from "react";
+import { Box, Typography, Divider } from "@mui/material";
 
 const Introduction = () => {
   return (
-      <Box>
-            <Typography variant="h4" gutterBottom>
-                    Hello, I'm Michael Nusair!
-                          </Typography>
-                                <Typography variant="body1" gutterBottom>
-                                        I am a full-stack developer with 2 years of experience, specializing in TypeScript, ReactJS, MUI, Redux, RxJS, AngularJS, CesiumJS, NodeJS, ExpressJS, GraphQL, MongoDB, BrainJS, Docker, Compose, AWS, EC2, Elastic BS CGP, Firebase, Python, and Selenium.
-                                              </Typography>
-                                                    <Divider />
-                                                          <Typography variant="h6" gutterBottom>
-                                                                  Work Experience:
-                                                                        </Typography>
-                                                                              <Typography variant="body1" gutterBottom>
-                                                                                      Full-Stack Developer at Ofek 324 (2022-2023)
-                                                                                            </Typography>
-                                                                                                  <Typography variant="body1" gutterBottom>
-                                                                                                          CTO at NeurobiCare Ltd, Neurobica LLC (2022)
-                                                                                                                </Typography>
-                                                                                                                      <Typography variant="body1" gutterBottom>
-                                                                                                                              Product Manager and Air Control Officer at IAF – Air Control HQ (2018-2021)
-                                                                                                                                    </Typography>
-                                                                                                                                          <Divider />
-                                                                                                                                                <Typography variant="h6" gutterBottom>
-                                                                                                                                                        Education:
-                                                                                                                                                              </Typography>
-                                                                                                                                                                    <Typography variant="body1" gutterBottom>
-                                                                                                                                                                            Open University: Computer Science B.Sc Student
-                                                                                                                                                                                  </Typography>
-                                                                                                                                                                                        <Typography variant="body1" gutterBottom>
-                                                                                                                                                                                                See-Security Collage: Cybersecurity Intro and Network Administration + SOC Tier 1 Certified
-                                                                                                                                                                                                      </Typography>
-                                                                                                                                                                                                            <Typography variant="body1" gutterBottom>
-                                                                                                                                                                                                                    LPI: Linux Essentials Certified
-                                                                                                                                                                                                                          </Typography>
-                                                                                                                                                                                                                                <Divider />
-                                                                                                                                                                                                                                      <Typography variant="h6" gutterBottom>
-                                                                                                                                                                                                                                              Contact Information:
-                                                                                                                                                                                                                                                    </Typography>
-                                                                                                                                                                                                                                                          <Typography variant="body1" gutterBottom>
-                                                                                                                                                                                                                                                                  Email: Mic.n.104@gmail.com
-                                                                                                                                                                                                                                                                        </Typography>
-                                                                                                                                                                                                                                                                              <Typography variant="body1" gutterBottom>
-                                                                                                                                                                                                                                                                                      Phone: +972528971871
-                                                                                                                                                                                                                                                                                            </Typography>
-                                                                                                                                                                                                                                                                                                </Box>
-                                                                                                                                                                                                                                                                                                  );
-                                                                                                                                                                                                                                                                                                  };
+    <Box>
+      <Typography variant="h4" gutterBottom>
+        Hello, I'm Michael Nusair!
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        I am a full-stack developer with 2 years of experience, specializing in
+        TypeScript, ReactJS, MUI, Redux, RxJS, AngularJS, CesiumJS, NodeJS,
+        ExpressJS, GraphQL, MongoDB, BrainJS, Docker, Compose, AWS, EC2, Elastic
+        BS CGP, Firebase, Python, and Selenium.
+      </Typography>
+      <Divider />
+      <Typography variant="h6" gutterBottom>
+        Work Experience:
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Full-Stack Developer at Ofek 324 (2022-2023)
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        CTO at NeurobiCare Ltd, Neurobica LLC (2022)
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Product Manager and Air Control Officer at IAF – Air Control HQ
+        (2018-2021)
+      </Typography>
+      <Divider />
+      <Typography variant="h6" gutterBottom>
+        Education:
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Open University: Computer Science B.Sc Student
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        See-Security Collage: Cybersecurity Intro and Network Administration +
+        SOC Tier 1 Certified
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        LPI: Linux Essentials Certified
+      </Typography>
+      <Divider />
+      <Typography variant="h6" gutterBottom>
+        Contact Information:
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Email: Mic.n.104@gmail.com
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Phone: +972528971871
+      </Typography>
+    </Box>
+  );
+};
 
-                                                                                                                                                                                                                                                                                                  export default Introduction;
-
-
-                                                                                                                                                                                                                                                                                                  export dwfault Introduction
+export default Introduction;
