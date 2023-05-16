854782699033cd2084e40901d22894065c9f19a1
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-05-16 20:38:03 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { Typography, Box } from "@mui/material";
+import {
+  Typography,
+  Box,
+  Grid,
+  Card,
+  CardContent,
+  Button,
+} from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 // Define a custom styled component for the root container
@@ -19,66 +26,57 @@ const StyledMainContent = styled(Box)({
   margin: "0 auto",
 });
 
+const StyledCard = styled(Card)({
+  minWidth: "275px",
+  margin: "1rem",
+  padding: "1rem",
+  textAlign: "center",
+});
+
 const App = () => {
   return (
     <StyledContainer>
       <StyledMainContent>
-        <Typography variant="h3">Michael Nusair</Typography>
-        <Typography variant="subtitle1">Full-Stack Developer</Typography>
-        <Typography variant="body1">Contact Details:</Typography>
-        <Typography variant="body2">
-          +972528971871 Mic.n.104@gmail.com
+        <Typography variant="h3" align="center">
+          Michael Nusair
         </Typography>
-        <Typography variant="body1">Skills:</Typography>
-        <ul>
-          <li>TypeScript</li>
-          <li>ReactJS, MUI Redux, RxJS AngularJS, CesiumJS NodeJS</li>
-          <li>ExpressJS, GraphQL MongoDB</li>
-          <li>BrainJS</li>
-          <li>
-            Docker, Compose AWS, EC2, Elastic BS CGP, Firebase Python, Selenium
-          </li>
-        </ul>
-        <Typography variant="body1">Education:</Typography>
-        <Typography variant="body2">
-          Open University: Computer Science B.Sc Student
+        <Typography variant="subtitle1" align="center">
+          Full-Stack Developer
         </Typography>
-        <Typography variant="body2">
-          See-Security Collage: Cybersecurity Intro and Network Administration +
-          SOC Tier 1 Certified
-        </Typography>
-        <Typography variant="body2">LPI: Linux Essentials Certified</Typography>
-        <Typography variant="body1">Languages:</Typography>
-        <ul>
-          <li>English - Native</li>
-          <li>Hebrew - Native</li>
-        </ul>
-        <Typography variant="body1">About me:</Typography>
-        <Typography variant="body2">
-          I am a full-stack developer with 2 years experience, and I am looking
-          for a challenging web development position. Learned independently and
-          quickly, experienced with project managing from conception to
-          fruition, alongside developing my own projects with true
-          entrepreneurial spirit and a lot of motivation.
-        </Typography>
-        <Typography variant="body1">Past Experience:</Typography>
-        <ul>
-          <li>Full-Stack Developer, IAF – Ofek 324: 2022-2023</li>
-          <li>
-            Developed an operational arial picture and air defense control
-            system at Ofek 324 Unit. The system has a Web UI and based on more
-            than 10 NodeJS microservices. Spent most of the time on the client,
-            a map-based application that uses Cesium and later refactored from
-            Angular to React. Built UIs with React MUI and used GraphQL to
-            contact most of the servers.
-          </li>
-          <li>Developed my own project for the IAF: 2021-2022</li>
-          <li>
-            Developing HR managing App, made with MERN stack. Deploying the
-            app’s server on Heroku, client on Netlify and database on Atlas for
-            tests and in the IAF network (OpenShift) for production
-          </li>
-        </ul>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <StyledCard>
+              <CardContent>
+                <Typography variant="h5">Web Development</Typography>
+                <Typography variant="body1">
+                  I offer professional and responsive web development services.
+                  I utilize my skills in TypeScript, ReactJS, Redux, RxJS,
+                  AngularJS, NodeJS, ExpressJS, MongoDB, and GraphQL to deliver
+                  top-notch websites.
+                </Typography>
+                <Button variant="contained" color="primary">
+                  Order Now
+                </Button>
+              </CardContent>
+            </StyledCard>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <StyledCard>
+              <CardContent>
+                <Typography variant="h5">App Development</Typography>
+                <Typography variant="body1">
+                  I offer high-quality app development services. My expertise in
+                  Docker, AWS, Firebase, Python, and Selenium, coupled with a
+                  keen entrepreneurial spirit, allows me to deliver great
+                  applications that meet your needs.
+                </Typography>
+                <Button variant="contained" color="primary">
+                  Order Now
+                </Button>
+              </CardContent>
+            </StyledCard>
+          </Grid>
+        </Grid>
       </StyledMainContent>
     </StyledContainer>
   );
